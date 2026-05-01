f868f904537c4ed600e13ed4a060f70ff480edb1
Gustavo Faustino <32557780+GustavoFaustinoDeAzevedo@users.noreply.github.com>
2026-05-01 10:12:10 -0300

    refactor(frontend): new login page and styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
-
 import './App.css';
 import { Login } from './pages/Login';
 
 function App() {
-  return <Login />;
+  return (
+    <div className="app__container">
+      <header></header>
+      <main>
+        <h1>Teste de Login</h1>
+        <Login />
+      </main>
+      <footer></footer>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { useAuth } from '../auth/useAuth';
 
+import './login.css';
+
 export const Login = () => {
   const { login } = useAuth();
 
@@ -12,14 +14,24 @@ export const Login = () => {
   }
 
   return (
-    <div>
-      <input title="email" onChange={(e) => setEmail(e.target.value)} />
+    <div className="login__container">
+      <input
+        title="email"
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        className="login__email"
+        required
+      />
       <input
         title="password"
         type="password"
+        placeholder="Password"
         onChange={(e) => setPassword(e.target.value)}
+        className="login__password"
+        required
       />
-      <button type="button" onClick={handleLogin}>
+      <button type="button" onClick={handleLogin} className="login__button">
         Login
       </button>
     </div>
